d971b77f72de917f35a37be6b21505ed9c415860
RaphaCardoso <154370014+RaphaCardoso@users.noreply.github.com>
2024-04-10 14:05:27 -0300

    Create locationController.js

---
+++ ProjLocal/src/controller/locationController.js
@@ -0,0 +1,33 @@
+const locationModel = require('../model/locationModel');
+
+exports.getAllLocation = async (req, res) => {
+    try {
+        const location = await locationModel.getAllLocation();
+        res.render("listLocation", { location: location });
+    } catch (err) {
+        res.status(500).json({ error: err.toString() });
+    }
+};
+
+exports.getContact = async (req, res) => {
+    try {
+        res.render("contact");
+    } catch (err) {
+        res.status(500).json({ error: err.toString() })
+    }
+};
+exports.getHome = async (req, res) => {
+    try {
+        res.render("home");
+    } catch (err) {
+        res.status(500).json({ error: err.toString() })
+    }
+};
+
+exports.getForm = async (req, res) => {
+    try {
+        res.render("form");
+    } catch (err) {
+        res.status(500).json({ error: err.toString() })
+    }
+};
