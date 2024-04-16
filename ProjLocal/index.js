0c0b7e1647c377ee474f5b701e1e5d330deb802a
RaphaCardoso <154370014+RaphaCardoso@users.noreply.github.com>
2024-04-16 16:42:31 -0300

    Update index.js

--- ProjLocal/index.js
+++ ProjLocal/index.js
@@ -1,14 +1,16 @@
 // Puxando express
 const express = require('express');
 const app = express();
-const port = 3002; 
-const router = require('./src/router/router'); 
-
+const port = 3002;
+const router = require('./src/router/router');
+const bodyParserReq = require('body-parser');
+const bodyParser = bodyParserReq;
 app.set('view engine', 'ejs');
 app.set('views', 'src/view/');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.use(express.json());
 app.use('/', router);
