f77b77ffbdae6c7057413a0d27099c406a408a00
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-04-24 13:50:58 -0300

    Update locationController.js

--- ProjLocal/src/controller/locationController.js
+++ ProjLocal/src/controller/locationController.js
@@ -32,21 +32,35 @@ exports.getForm = async (req, res) => {
     }
 };
 
-exports.newContact = async (req, res) => {
+exports.getForm = async (req, res) => {
     try {
-        const data = req.body
-        const location = await locationModel.createLocation(data);
         res.render("form");
     } catch (err) {
         res.status(500).json({ error: err.toString() })
     }
+};
 
-    /*
-    
-        locationModel.writeLocationToFile(data);
-    
-        res.render("form");
-    */
+exports.newContact = async (req, res) => {
+
+    // pegar os dados inseridos no form e guardar na const data
+    const data = req.body
+
+    try {
+
+        // como verifica é assincrona é necessário tratar a promessa utilizando await para retornar de maneira correta
+        const num = await locationModel.verifica(data);
+
+        // verifica o retorno e salva ou retorna erro
+        if (num === 0) {
+            locationModel.createLocation(data);
+            res.render("form");
+        } else if (num === 1) {
+            res.render("home");
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        res.status(500).send('Erro interno no servidor');
+    }
 
 };
 
